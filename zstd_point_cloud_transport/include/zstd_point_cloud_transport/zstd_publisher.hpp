5ce3fd7f360a205201de350a86cb147c384f325c
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-02-19 14:57:08 +0100

    Revert "Trimmed the branch to only the point_cloud_interfaces sub directory"
    This reverts commit b9fe4a6fffc9f710c87870098a20da27987e812f.

---
+++ zstd_point_cloud_transport/include/zstd_point_cloud_transport/zstd_publisher.hpp
@@ -0,0 +1,74 @@
+// Copyright (c) 2023, Open Source Robotics Foundation, Inc.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef ZSTD_POINT_CLOUD_TRANSPORT__ZSTD_PUBLISHER_HPP_
+#define ZSTD_POINT_CLOUD_TRANSPORT__ZSTD_PUBLISHER_HPP_
+
+#include <zstd.h>
+
+#include <memory>
+#include <string>
+
+#include <sensor_msgs/msg/point_cloud2.hpp>
+
+#include <point_cloud_transport/point_cloud_transport.hpp>
+
+#include <point_cloud_transport/simple_publisher_plugin.hpp>
+#include <point_cloud_interfaces/msg/compressed_point_cloud2.hpp>
+
+
+namespace zstd_point_cloud_transport
+{
+
+class ZstdPublisher
+  : public point_cloud_transport::SimplePublisherPlugin<
+    point_cloud_interfaces::msg::CompressedPointCloud2>
+{
+public:
+  ZstdPublisher();
+
+  std::string getTransportName() const override;
+
+  void declareParameters(const std::string & base_topic) override;
+
+  std::string getDataType() const override;
+
+  TypedEncodeResult encodeTyped(const sensor_msgs::msg::PointCloud2 & raw) const override;
+
+private:
+  int encode_level_{7};
+
+  // When compressing many times,
+  // it is recommended to allocate a context just once,
+  // and re-use it for each successive compression operation.
+  // This will make workload friendlier for system's memory.
+  ZSTD_CCtx * zstd_context_{nullptr};
+};
+}  // namespace zstd_point_cloud_transport
+
+#endif  // ZSTD_POINT_CLOUD_TRANSPORT__ZSTD_PUBLISHER_HPP_
