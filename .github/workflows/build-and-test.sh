34aa046b4fb23d236748e124fb5f9e62b1c68504
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-06-15 10:03:26 +0200

    Rebase from 'upstream'

--- .github/workflows/build-and-test.sh
+++ .github/workflows/build-and-test.sh
@@ -8,15 +8,14 @@ export DEBIAN_FRONTEND=noninteractive
 export ROS_PYTHON_VERSION=3
 
 apt update -qq
-apt install -qq -y lsb-release wget curl build-essential
+apt install -qq -y lsb-release wget curl build-essential gnupg
 
 # Dependencies.
-echo "deb http://packages.ros.org/ros2-testing/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-testing.list
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | gpg --dearmor > /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2-testing/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-testing.list
 apt-get update -qq
 apt-get install -y python3-colcon-common-extensions \
                    python3-rosdep python3-vcstool python3-vcstools
-
 rosdep init
 rosdep update
 
